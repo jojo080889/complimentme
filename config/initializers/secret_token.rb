eac35f93edd3bc8d52eb3ca6c677e9368d9a6fb4
Joy Kim <jojo080889@gmail.com>
2015-05-18 00:18:19 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Complimentme::Application.config.secret_key_base = 'f0429622be9ae227b85a2f8e18db289a25cc5a1ddd731c4cf699002ee55cb072bfe746f4236140758d453ae09eeb94d6244b1cadd85784759fcc1e824478cebf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Complimentme::Application.config.session_store :cookie_store, key: '_complimentme_session'
